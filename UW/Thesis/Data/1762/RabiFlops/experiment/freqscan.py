dd20063e215d512b7af9ed0ebdb89b29fefa0507
dre <aj90008@gmail.com>
2015-07-13 15:08:14 -0700

    white space/ syntax
    fixed the white space issues causing problems, and the syntax errors.
    Also added comments to make code purpose clearer

--- UW/Thesis/Data/1762/RabiFlops/experiment/freqscan.py
+++ UW/Thesis/Data/1762/RabiFlops/experiment/freqscan.py
@@ -112,41 +112,42 @@ class Experiment:
 			#CHANGES BEING MADE BELOW-----------------
 			##########################################			
             while experiment.step( freq_src, ni ):
-				nsucc = [0 for i in ion_positions] #should hold # of successes for each ion
-				indicator = 0 #new code
-				while indicator<nruns: #new code
+                nsucc = [0 for i in ion_positions] # number of successes for each ion
+                indicator = 0 #number of time it has run
+		while indicator<nruns: #allows dynamic nruns switching
                 #for i in range( nruns ): #old code
                     data = self.build_data( 
                         camera, ion_positions, camera.get_image() )
                     ion_order = [ d > threshold for d in data ]
 					
-					#--------------NEW CODE-----------------------
-					for i in  range(len(nsucc)):
-						if ion_order[i]:
-							nsucc[i]+=1 #counts number of successes (bright)
+		    #--------------NEW CODE-----------------------
+		    for i in  range(len(nsucc)):
+			if ion_order[i]:
+			    nsucc[i]+=1 #counts number of successes (bright)
 					
 					
-					#NOTE: LOOK INTO POSSIBILITY OF USING A DICTIONARY HERE
-					if indicator+1==20: #since it starts at 0 need to add 1
-						nsucc = [x/float(indicator+1) for x in nsucc]
-						props = [np.abs(0.5 - x for x in nsucc] 
-						#calculates proportion of successes of the 20 runs
-						#and subtracts from 0.5 so that we can find which is closest
-						minp = props[0]
-						pHat = nsucc[0]
-						for i in range(len(nsucc)):
-							if minp > props[i]
-								minp = props[i]
-								pHat = nsucc[i]
-						bStdE = binStdrderr(indicator+1,pHat)
-						if bStdE<0.05:
-							nruns = 20 #this will force the program to break out of this while loop
-						else:
-							nruns = 50
-								
-					
-						
-					#---------------end new code section--------------------
+		    #NOTE: LOOK INTO POSSIBILITY OF USING A DICTIONARY HERE
+		    if indicator+1==20: #since it starts at 0 need to add 1
+			nsucc = [x/float(indicator+1) for x in nsucc] #array holding proportion of successes for each ion respectively
+			props = [np.abs(0.5 - x) for x in nsucc] 
+			#props holds the value = proportion of successes of the 20 runs
+			#and subtracts from 0.5 so that we can find which is closest to 50%
+			minp = props[0] #minimum modified proportions (the smallest is the one closest to 50%
+                        pHat = nsucc[0] #this is the actual proportion we want (could replace this variable
+                        #using props[0]+.5
+                        #--------finds min------------------
+			for i in range(len(nsucc)):
+			    if minp > props[i]:
+				minp = props[i]
+				pHat = nsucc[i]
+			bStdE = self.binStdrderr(indicator+1,pHat) #finds binomial standard error
+			if bStdE<0.05:
+			    nruns = 20 #this will force the program to break out of this while loop
+			    print bStdE, nruns
+			else:
+			    nruns = 50
+			    print bStdE, nruns
+		    #---------------end new code section--------------------
                     bg.append( data[-1] )
                     if len( bg ) > 1000:
                         bg = bg[100:]
@@ -205,7 +206,7 @@ class Experiment:
                     d.close()
 
                     time.sleep( 0.2 )
-					indicator+=1 #new code
+		    indicator+=1 #new code
               
               
         finally:
@@ -223,11 +224,11 @@ class Experiment:
             data.append( val )
         return data
 	
-	def binStdrderr(n,p,z=1):
-	#finds standard error for a binomial distribution
-	#n = # of runs, p = proportion of successes
-		se = z*np.sqrt((1/float(n))*p*(1-p)+(1/float(4*n*n))*z*z)	
-		return se
+    def binStdrderr(self, n,p,z=1.0):
+    #finds standard error for a binomial distribution
+    #n = # of runs, p = proportion of successes
+        se = z*np.sqrt((1/(n*1.0))*p*(1-p)+(1/(4.0*n*n))*z*z)
+        return se
 
 if __name__ == '__main__':
     import sys
